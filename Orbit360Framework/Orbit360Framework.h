3279337f08922cebc40afa99a589b8e373566fdb
Philipp Meyer <philipp-meyer-freital@gmx.de>
2017-08-18 17:09:31 +0200

    Initial Commit

---
+++ Orbit360Framework/Orbit360Framework.h
@@ -0,0 +1,19 @@
+//
+//  Orbit360Framework.h
+//  Orbit360Framework
+//
+//  Created by Philipp Meyer on 18.08.17.
+//  Copyright © 2017 Philipp Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Orbit360Framework.
+FOUNDATION_EXPORT double Orbit360FrameworkVersionNumber;
+
+//! Project version string for Orbit360Framework.
+FOUNDATION_EXPORT const unsigned char Orbit360FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Orbit360Framework/PublicHeader.h>
+
+
